8df8b9cb5727677c615ba8f46d61ec1dddb34984
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-03-08 13:45:05 +0300

    Update nabu-rootfs_build.sh

--- nabu-rootfsKernel_build.sh
+++ nabu-rootfsKernel_build.sh
@@ -8,29 +8,6 @@ fi
 
 VERSION="23.10"
 
-#DEPS dpkg, wget, binfmt support(or arm64 device), 7zip, make, aarch64 gcc,
-#DEPS openssl headers, bc, bison, flex, bash, kmod, cpio, binutils, tar, git
-
-git clone https://github.com/map220v/sm8150-mainline.git --branch nabu-$2 --depth 1 linux
-cd linux
-sed -i 'CONFIG_CMDLINE="quiet splash"' ./arch/arm64/configs/sm8150.config
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig sm8150.config
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
-mkdir ../linux-xiaomi-nabu/boot
-cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$2.0-sm8150
-cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$2.0-sm8150
-rm -rf ../linux-xiaomi-nabu/lib
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=../linux-xiaomi-nabu modules_install
-rm ../linux-xiaomi-nabu/lib/modules/**/build
-cd ..
-rm -rf linux
-
-dpkg-deb --build --root-owner-group linux-xiaomi-nabu
-dpkg-deb --build --root-owner-group firmware-xiaomi-nabu
-dpkg-deb --build --root-owner-group alsa-xiaomi-nabu
-
-
-
 truncate -s 6G rootfs.img
 mkfs.ext4 rootfs.img
 mkdir rootdir
