659c8cae81c56b9936badc16f350f62bbec79303
map220v <map220v300@gmail.com>
2024-04-03 20:36:08 +0300

    Fix kernel package versioning

--- nabu-kernel_build.sh
+++ nabu-kernel_build.sh
@@ -1,11 +1,12 @@
 git clone https://github.com/map220v/sm8150-mainline.git --branch nabu-$1 --depth 1 linux
 cd linux
-sed -i 'CONFIG_CMDLINE="quiet splash"' ./arch/arm64/configs/sm8150.config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig sm8150.config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+_kernel_version="$(make kernelrelease -s)"
 mkdir ../linux-xiaomi-nabu/boot
-cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$2.0-sm8150
-cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$2.0-sm8150
+cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$_kernel_version
+cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$_kernel_version
+sed -i "s/Version:.*/Version: ${_kernel_version}/" ../linux-xiaomi-nabu/DEBIAN/control
 rm -rf ../linux-xiaomi-nabu/lib
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=../linux-xiaomi-nabu modules_install
 rm ../linux-xiaomi-nabu/lib/modules/**/build
